3bf3b83f2aa7eed40591453e43561d429afcae99
Ashish <ashm@stanford.edu>
2019-11-11 14:06:11 -0800

    removed requirements file since I'm using install file now

--- requirements.R
+++
@@ -1,9 +0,0 @@
-install.packages("devtools") 
-devtools::install_github("TomHardwicke/ReproReports")
-install.packages("tidyverse") # for data munging
-install.packages("knitr") # for kable table formating
-install.packages("haven") # import and export 'SPSS', 'Stata' and 'SAS' Files
-install.packages("readxl") # import excel files
-install.packages("lsr") # calculate partial eta squared
-install.packages("here") # root file path
-install.packages("pander")
